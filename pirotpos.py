da86de820c8f0bb18f9baa6c2bf78ba9f42c70fc
Tommi tofthøj <ttofthoj@gmail.com>
2022-05-14 12:31:29 +0200

    add socket

--- pirotpos.py
+++ pirotpos.py
@@ -4,9 +4,12 @@ import socket
 
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("192.168.249.189", 1234))
-msg = s.recv(1024)
-print(msg.decode("utf-8"))
+s.bind((socket.gethostname(), 1234))
+s.listen(5)
+while True:
+    clientsocket, address = s.accept()
+    print(f"connection from {address} has been achieved")
+    clientsocket.send(bytes("Welcome", "utf-8"))
 while True:
     pass
 try:

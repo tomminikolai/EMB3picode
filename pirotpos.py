6e1b7048d75b483a7351672872a04b53ee92bf85
Tommi tofthøj <ttofthoj@gmail.com>
2022-05-14 13:34:05 +0200

    add socket

--- pirotpos.py
+++ pirotpos.py
@@ -1,7 +1,7 @@
 import serial
 import time
 import socket
-
+import math
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((socket.gethostname(), 1234))
@@ -20,17 +20,29 @@ except serial.serialutil.SerialException:
 mtr1 = 0
 mtr2 = 0
 to = 0
+i = 0
+pos = [750, 500]
+theta = 0
 running = True
 while running:
     try:
         startmsg = ser.read(1)
         if startmsg == b'\x0F':
-            msg = ser.read(2)
+            mtr1 += ser.read(2)
         elif startmsg == b'\xF0':
-            msg = ser.read(2)
-        if startmsg:
-            clientsocket.send(startmsg)
-            clientsocket.send(msg)
+            mtr2 += ser.read(2)
+        if i >= 10:
+            i = 0
+            theta += (mtr1 - mtr2) / 12
+            dx = ((mtr1 + mtr2) / 2) * math.cos(theta)
+            dy = ((mtr1 + mtr2) / 2) * math.sin(theta)
+            pos[0] += dx
+            pos[1] += dy
+            clientsocket.send(int(pos[0]).to_bytes(4, "big"))
+            clientsocket.send(int(pos[1]).to_bytes(4, "big"))
+            mtr1 = 0
+            mtr2 = 0
+        i += 1
     except KeyboardInterrupt:
         running = False
 ser.close()

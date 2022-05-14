9324d4e1835b1d95f05becf7045175aef5bb72ea
Tommi tofthøj <ttofthoj@gmail.com>
2022-05-14 12:33:49 +0200

    add socket

--- pirotpos.py
+++ pirotpos.py
@@ -6,10 +6,9 @@ import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((socket.gethostname(), 1234))
 s.listen(5)
-while True:
-    clientsocket, address = s.accept()
-    print(f"connection from {address} has been achieved")
-    clientsocket.send(b'\x0F')
+clientsocket, address = s.accept()
+print(f"connection from {address} has been achieved")
+clientsocket.send(b'\x0F')
 while True:
     pass
 try:
